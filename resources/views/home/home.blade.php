9d789b1a407f03c044e512e963aa3068f673cee5
azharanowar <azharanowar@gmail.com>
2022-11-25 11:15:58 +0600

    Link href to using route() instead of url

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -11,23 +11,26 @@
 </head>
 <body>
 
-    <header class="bg-dark py-2">
+    <header class="bg-dark py-3">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <nav class="navbar navbar-dark navbar-expand-lg">
                         <div class="container-fluid">
-                            <a class="navbar-brand text-uppercase fw-normal" href="#"><span class="text-primary">Logo</span> Here</a>
+                            <a class="navbar-brand text-uppercase fw-normal" href="{{ route('home') }}"><span class="text-primary">Logo</span> Here</a>
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
                             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                                 <ul class="navbar-nav ms-auto mb-2 me-3 mb-lg-0">
                                     <li class="nav-item">
-                                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                        <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">About Us</a>
+                                        <a class="nav-link" href="#">Add Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Manage Blog</a>
                                     </li>
                                 </ul>
                                 <form class="d-flex" role="search">
