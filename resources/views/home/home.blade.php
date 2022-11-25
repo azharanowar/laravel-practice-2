2aaf43e9cf156c35412e35fe72b58feabed49b79
azharanowar <azharanowar@gmail.com>
2022-11-25 11:07:23 +0600

    Header design completed with bootstrap nabvar

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -6,8 +6,42 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home | Laravel</title>
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/style.css"/>
 </head>
 <body>
 
+    <header class="bg-dark py-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar-dark navbar-expand-lg">
+                        <div class="container-fluid">
+                            <a class="navbar-brand text-uppercase fw-normal" href="#"><span class="text-primary">Logo</span> Here</a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav ms-auto mb-2 me-3 mb-lg-0">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">About Us</a>
+                                    </li>
+                                </ul>
+                                <form class="d-flex" role="search">
+                                    <input class="form-control me-1" type="search" placeholder="Search" aria-label="Search">
+                                    <button class="btn btn-primary" type="submit">Search</button>
+                                </form>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+
+<script src="{{ asset('assets') }}/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
