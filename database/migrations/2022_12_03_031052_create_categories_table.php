d44b4e21d92d5e17f52089564a02726a58d5b3c2
azharanowar <azharanowar@gmail.com>
2022-12-03 09:14:29 +0600

    Created table in database with php artisan mirgation

--- database/migrations/2022_12_03_031052_create_categories_table.php
+++ database/migrations/2022_12_03_031052_create_categories_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('category', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('category_name');
+            $table->tinyInteger('publication_status')->nullable(0);
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('category');
+        Schema::dropIfExists('categories');
     }
 };
